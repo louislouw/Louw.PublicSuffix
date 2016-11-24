a844913e150be9b92e9e4881b36e9a68e695a8e3
Louis Louw <accounts@louislouw.me>
2016-11-24 22:50:12 +0200

    DomainNameTest

---
+++ test/Louw.PublicSuffix.UnitTests/DomainNameTest.cs
@@ -0,0 +1,48 @@
+﻿using Xunit;
+
+namespace Louw.PublicSuffix.UnitTests
+{
+    public class DomainNameTest
+    {
+        [Fact]
+        public void CheckDomainName1()
+        {
+            var domainParser = new DomainParser();
+            domainParser.AddRule(new TldRule("com"));
+
+            var domainName = domainParser.Get("test.com");
+            Assert.Equal("test", domainName.Domain);
+            Assert.Equal("com", domainName.TLD);
+            Assert.Equal("test.com", domainName.RegistrableDomain);
+            Assert.Equal(null, domainName.SubDomain);
+        }
+
+        [Fact]
+        public void CheckDomainName2()
+        {
+            var domainParser = new DomainParser();
+            domainParser.AddRule(new TldRule("uk"));
+            domainParser.AddRule(new TldRule("co.uk"));
+
+            var domainName = domainParser.Get("test.co.uk");
+            Assert.Equal("test", domainName.Domain);
+            Assert.Equal("co.uk", domainName.TLD);
+            Assert.Equal("test.co.uk", domainName.RegistrableDomain);
+            Assert.Equal(null, domainName.SubDomain);
+        }
+
+        [Fact]
+        public void CheckDomainName3()
+        {
+            var domainParser = new DomainParser();
+            domainParser.AddRule(new TldRule("uk"));
+            domainParser.AddRule(new TldRule("co.uk"));
+
+            var domainName = domainParser.Get("sub.test.co.uk");
+            Assert.Equal("test", domainName.Domain);
+            Assert.Equal("co.uk", domainName.TLD);
+            Assert.Equal("test.co.uk", domainName.RegistrableDomain);
+            Assert.Equal("sub", domainName.SubDomain);
+        }
+    }
+}
