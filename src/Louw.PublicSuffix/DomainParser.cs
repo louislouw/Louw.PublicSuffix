0cc81ff901ed57aa47d5f495de4e04b9f2bfed1b
Louis Louw <accounts@louislouw.me>
2016-12-02 21:32:31 +0200

    removed unused usings

--- src/Louw.PublicSuffix/DomainParser.cs
+++ src/Louw.PublicSuffix/DomainParser.cs
@@ -1,10 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Globalization;
 using System.Linq;
-using System.Net.Http;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Louw.PublicSuffix
 {
